ff7eba513b1dfe3f6fe09f8ab62d02010f094b92
Thiago Moreira <thiago.m.hq@gmail.com>
2025-08-29 13:19:17 -0300

    Refactor code structure for improved readability and maintainability

--- firebase_config.ts
+++ firebase_config.ts
@@ -1,22 +1,22 @@
-import { initializeApp } from "firebase/app";
-import { initializeAuth } from "firebase/auth";
+import auth from '@react-native-firebase/auth';
+// import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyB1I9UNydrnxXrXmqzJx60ZWcngxV3MOW0",
-    authDomain: "fluxo-docs.firebaseapp.com",
-    projectId: "fluxo-docs",
-    storageBucket: "fluxo-docs.firebasestorage.app",
-    messagingSenderId: "817465462229",
-    appId: "1:817465462229:web:adf91c5270c0fdb2a5c15c",
-    measurementId: "G-ZNVM3HQM45"
-};
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const firebaseConfig = {
+//     apiKey: "AIzaSyB1I9UNydrnxXrXmqzJx60ZWcngxV3MOW0",
+//     authDomain: "fluxo-docs.firebaseapp.com",
+//     projectId: "fluxo-docs",
+//     storageBucket: "fluxo-docs.firebasestorage.app",
+//     messagingSenderId: "817465462229",
+//     appId: "1:817465462229:web:adf91c5270c0fdb2a5c15c",
+//     measurementId: "G-ZNVM3HQM45"
+// };
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
-const auth = initializeAuth(app, {
-    // persistence: getReactNativePersistence(ReactNativeAsyncStorage),
-});
+// // const auth = initializeAuth(app, {
+// //     // persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+// // });
 
-export { app, auth };
+export { auth };
 
 
